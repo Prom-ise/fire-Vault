e9f0d4ae891402f2e2b5e2149a93c219e9447ace
promise <emmanuelpromise456@gmail.com>
2024-02-25 17:17:59 +0100

    update 4 files and delete 46 files

---
+++ public/script.js
@@ -0,0 +1,13 @@
+// const input = document.querySelectorAll('input');
+
+// input.forEach((element) => {
+//   element.addEventListener('focus', () => {
+//     document.getElementById('stop1').setAttribute('stop-color', 'blue');
+//     document.getElementById('stop2').setAttribute('stop-opacity', '0');
+//   });
+
+//   element.addEventListener('blur', () => {
+//     document.getElementById('stop1').setAttribute('stop-color', 'blue');
+//     document.getElementById('stop2').setAttribute('stop-opacity', '0');
+//   });
+// });
